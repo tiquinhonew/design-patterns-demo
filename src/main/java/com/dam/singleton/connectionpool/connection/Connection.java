2668e132253b5b3578f074a96177bdb4333e8da5
Douglas Moraes <douglas.am@gmail.com>
2024-05-20 00:06:54 -0300

    Studying Singleton

---
+++ src/main/java/com/dam/singleton/connectionpool/connection/Connection.java
@@ -0,0 +1,23 @@
+package com.dam.singleton.connectionpool.connection;
+
+public class Connection {
+    private boolean inUse;
+
+    public Connection() {
+        inUse = false;
+    }
+
+    public Object query(String sql) {
+        System.out.println("Running '" + sql + "' in Connection " + this);
+        return null;
+    }
+
+    boolean isInUse() {
+        return inUse;
+    }
+
+    void setInUse(boolean status) {
+        inUse = status;
+    }
+}
+
